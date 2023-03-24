4a678f8f4ea1dc1421cf64a8c00239375e228aba
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-24 21:18:05 +0300

    create a new hero_power

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,15 @@
 class HeroPowersController < ApplicationController
+    def create
+        hero_power = HeroPower.new(hero_power_params)
+        if hero_power.save
+            render json: hero_power.hero
+        else
+            render json: { errors: "validation errors" }, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
 end
