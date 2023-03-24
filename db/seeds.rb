d96181283cc4684a31678a096b09a60e59b4d0cd
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-24 14:15:56 +0300

    migrations, seeding and validations

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+puts 'seeding...'
+
+10.times do
+  Power.create(
+    name: Faker::Superhero.power,
+    description: Faker::Hipster.sentence
+  )
+end
+
+10.times do
+  Hero.create(
+    name: Faker::Superhero.name,
+    super_name: Faker::Superhero.descriptor
+  )
+end
+
+strength_options = ['Strong', 'Weak', 'Average']
+
+10.times do
+  HeroPower.create(
+    strength: strength_options.sample,
+    hero_id: Hero.pluck(:id).sample,
+    power_id: Power.pluck(:id).sample
+  )
+end
+
+puts 'Done seeding!'
